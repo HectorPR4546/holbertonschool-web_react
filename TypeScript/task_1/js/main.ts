f5acf920c5573a9023b1cf8cec3e9c7802f05b85
HectorPR4546 <h_perez93@yahoo.com>
2025-09-13 01:23:26 -0400

    feat(task_1): add Teacher interface with readonly names and index signature; scaffold configs

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,20 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; // allow additional properties of any type
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+// eslint-disable-next-line no-console
+console.log(teacher3);
+
